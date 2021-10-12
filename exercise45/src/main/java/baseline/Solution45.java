db18a23b91f62104318b3b4d8433caa15e0f291f
DetrichLange <detrich.lange@gmail.com>
2021-10-12 11:57:38 -0400

    pseudocode for 45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Detrich Lange
+ */
+
+/*
+Given an input file named `exercise45_input.txt`, read the file and look for all occurrences of the word utilize.
+Replace each occurrence with use. Write the modified file to a new file.
+    Prompt for the name of the output file.
+    Write the output to a new file.
+ */
+
+package baseline;
+
+public class Solution45 {
+
+    //Prompt for the name of the output file
+    //Open input file and output file as a try with resources
+        //Make a StringBuilder outputString
+        //While there is more to read:
+            //Add the next line from the text file to the outputString
+        //Use regex to replaceAll the word "utilize" to "use" in the outputString
+        //Print the outputString to the output file
+    //Print confirmation that the replacement is done
+
+    public static void main(String[] args) {
+
+    }
+}
